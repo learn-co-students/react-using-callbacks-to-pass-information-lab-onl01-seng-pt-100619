9ce9f439a310643adfed74c73ec0e64531c2f241
Ann Duong <39737217+thuyanduong-flatiron@users.noreply.github.com>
2019-10-30 23:03:37 -0400

    Update Matrix.js

--- src/Matrix.js
+++ src/Matrix.js
@@ -18,10 +18,8 @@ export default class Matrix extends Component {
     })
   }
 
-  getSelectedColor = () => (this.state.selectedColor)
-
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} getSelectedColor={this.getSelectedColor} />) // replace me and render a cell component instead!
+    vals.map((val, idx) => <Cell key={idx} color={val} selectedColor={this.state.selectedColor} />) // replace me and render a cell component instead!
   )
 
   genMatrix = () => (
