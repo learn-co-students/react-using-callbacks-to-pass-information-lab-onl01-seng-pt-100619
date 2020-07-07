14dfa2fbfa068d214261fe5be4c74d9547d7914c
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-07-07 01:26:28 -0700

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -8,10 +8,16 @@ export default class Cell extends Component {
       color: this.props.color
     }
   }
+
+  handleClick = () => {
+    this.setState({
+      color: this.props.selectedColor
+    })
+  }
   
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      <div onClick={this.handleClick} className="cell" style={{backgroundColor: this.state.color}}>
       </div>
     )
   }
